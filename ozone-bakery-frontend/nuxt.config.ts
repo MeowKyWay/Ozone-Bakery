451afb177d462e6e6f61c78185f3b1506ccc50da
meow <69355934+MeowKyWay@users.noreply.github.com>
2023-10-27 13:39:01 +0700

    Add module and setup

--- ozone-bakery-frontend/nuxt.config.ts
+++ ozone-bakery-frontend/nuxt.config.ts
@@ -1,4 +1,23 @@
+import portcss from "postcss";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: [
+    '~/assets/css/main.css',
+  ],
+
+  modules: [
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+		'@pinia-plugin-persistedstate/nuxt',
+  ],
+
 })
---
+++ ozone-bakery-frontend/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <p class="text-xl font-bold bg-red-500">รักเธอ</p>
+</template>
