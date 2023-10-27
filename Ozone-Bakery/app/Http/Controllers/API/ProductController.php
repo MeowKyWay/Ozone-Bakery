1a25003dbb6e337acf31ab59e87e478f5048da2f
meow <69355934+MeowKyWay@users.noreply.github.com>
2023-10-27 23:43:42 +0700

    Fixed product

--- Ozone-Bakery/app/Http/Controllers/API/ProductController.php
+++ Ozone-Bakery/app/Http/Controllers/API/ProductController.php
@@ -30,8 +30,11 @@ class ProductController extends Controller
         $product = new Product();
 
         $product->name = $request->get('name');
-        $product->amount = $request->get('price');
-        $product->image_path = $request->get('image_path');
+        $product->price = $request->get('price');
+        if ($request->has('image_path')) $product->image_path = $request->get('image_path');
+        else $product->image_path = "Ozone-Bakery/public/image/product/default.jpg";
+        if ($request->has('description')) $product->description = $request->get('description');
+        else $product->description = "null";
 
         $product->save();
         $product->refresh();
@@ -43,11 +46,12 @@ class ProductController extends Controller
         $request->validate ([
             'name' => 'nullable|string|min:3',
             'price' => 'nullable|integer|min:0',
-            'image_path' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048'
+            'image_path' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'description' => 'nullable|string',
         ]);
 
         if ($request->has('name')) $product->name = $request->get('name');
-        if ($request->has('price')) $product->amount = $request->get('price');
+        if ($request->has('price')) $product->price = $request->get('price');
         if ($request->has('image_path')) $product->image_path = $request->get('image_path');
         $product->save();
         $product->refresh();
---
+++ ozone-bakery-frontend/vite.config.ts
@@ -0,0 +1,8 @@
+// vite.config.ts
+import { UserConfig } from 'vite';
+
+const config: UserConfig = {
+  // Configure Vite options here
+};
+
+export default config;
